192189a58337c2f81cedaf1f24526ab012ef730e
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-09-14 21:18:33 -0400

    Jumbotron component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./components/Navbar.js"
+import Jumbotron from "./components/Jumbotron.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <div className="container">
-          <div className="jumbotron">
-          <h1><b>Vote</b></h1>
-          <p>A place to host polls and vote on them.</p>
-          <button className="btn btn-primary btn-lg">Login</button>
-      </div>
-    </div>
+        <Jumbotron />
       </div>
     );
   }
