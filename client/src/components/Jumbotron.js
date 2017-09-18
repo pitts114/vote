5f795eefa97a955b76e5545a7c3c813108211e12
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-09-18 00:14:18 -0400

    Navbar fix

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -3,13 +3,11 @@ import React, {Component} from "react"
 class Jumbotron extends Component {
   render() {
     return(
-      <div className="animated fadeInUp">
-        <div className="jumbotron">
+        <div className="jumbotron animated fadeInUp">
           <h1><b>Vote</b></h1>
           <p>A place to host polls and vote on them.</p>
           <button className="btn btn-primary btn-lg">Login</button>
         </div>
-      </div>
     )
   }
 }
