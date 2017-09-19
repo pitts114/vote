c1c01b474eefb015de779040d299b0d79b031dab
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-09-18 21:37:17 -0400

    Add new poll form

--- client/src/components/NewPollPage.js
+++ client/src/components/NewPollPage.js
@@ -3,7 +3,21 @@ import React, {Component} from "react"
 class NewPollPage extends Component {
   render(){
     return(
-      <div></div>
+      <div className="col-xs-12 col-sm-8 col-sm-offset-2 NewPollPage">
+        <h1 className="text-center text-white">Create A New Poll</h1>
+        <div className="well">
+          <form>
+            <div className="form-group">
+              <label className="control-label text-dark">Poll Title</label>
+              <input type="text" className="form-control" placeholder="Poll Title" />
+              <br/>
+              <label className="control-label text-dark">Poll Options (Comma Seperated)</label>
+              <input type="text" className="form-control" placeholder="Option 1, Option 2, etc." />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
     )
   }
 }
