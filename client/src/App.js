74f1b9697d0e5271df5ef5ba40d95e4914b62e49
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-09-18 21:09:00 -0400

    Update render page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from "./components/Navbar.js"
 import Frontpage from "./components/Frontpage.js"
 import PollPage from "./components/PollPage.js"
+import NewPollPage from "./components/NewPollPage.js"
 
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
     }
     this.featurePoll = this.featurePoll.bind(this)
     this.goHome=this.goHome.bind(this)
+    this.goNewPoll = this.goNewPoll.bind(this)
   }
 
   featurePoll(pollId) {
@@ -27,23 +29,35 @@ class App extends Component {
     this.setState(state)
   }
 
+  goNewPoll(){
+    const state = this.state
+    state.actovePage="New"
+    this.setState(state)
+  }
+
   render() {
-    if (this.state.activePage === "Home")
-    return (
+    var page
+
+    switch (this.state.activePage){
+      case "Home":
+        page = <Frontpage featurePoll={this.featurePoll}/>
+        break
+      case "New":
+        page = <NewPollPage />
+        break
+      default:
+        page = <PollPage poll={this.state.activePage} goHome={this.goHome}/>
+        break
+    }
+
+    return(
       <div className="App">
         <Navbar goHome={this.goHome} active={this.state.activePage}/>
-        <Frontpage featurePoll={this.featurePoll}/>
+        {page}
       </div>
-    );
-    else {
-      return(
-        <div className="App">
-          <Navbar goHome={this.goHome} active={this.state.activePage}/>
-          <PollPage poll={this.state.activePage} goHome={this.goHome}/>
-        </div>
-      )
-    }
+    )
   }
 }
+
 
 export default App;
---
+++ client/src/components/NewPollPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react"
+
+class NewPollPage extends Component {
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default NewPollPage
