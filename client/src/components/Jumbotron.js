dce2ee2efd9f2a41089e494ba9381b628d533bcf
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-09-19 23:01:05 -0400

    Remove center login button for now

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -3,10 +3,16 @@ import React, {Component} from "react"
 class Jumbotron extends Component {
   render() {
     return(
+      /*
         <div className="jumbotron animated fadeInUp text-center Jumbotron">
           <h1><b>Vote</b></h1>
           <p>A place to host polls and vote on them.</p>
           <button className="btn btn-primary btn-lg">Login</button>
+        </div>
+        */
+        <div className="jumbotron animated fadeInUp text-center Jumbotron">
+          <h1><b>Vote</b></h1>
+          <p>A place to host polls and vote on them.</p>
         </div>
     )
   }
