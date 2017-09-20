bba8589eed609ccb49bfbaf1e4b833fc55a6c89f
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-09-20 18:45:20 -0400

    Style Login Page

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -3,10 +3,10 @@ import React, {Component} from "react"
 class LoginPage extends Component {
   render() {
     return (
-      <div className="container Page">
-        <h1>Login</h1>
+      <div className="container Page text-center">
+        <h1 className="text-white">Login</h1>
         <form action="/auth/github">
-          <input type="submit" value="Login with GitHub" />
+          <input className="btn btn-primary" type="submit" value="Login with GitHub" />
         </form>
       </div>
     )
