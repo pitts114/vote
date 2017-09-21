c70c0e73f1772a7e45859d782d20937bb7ed5c09
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-09-21 00:01:45 -0400

    Add example carousel

--- client/src/pages/Frontpage.js
+++ client/src/pages/Frontpage.js
@@ -7,7 +7,44 @@ class Frontpage extends Component{
     return(
       <div className="container">
         <Jumbotron />
-        <FrontpagePolls/>
+        <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+
+  <ol className="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+
+  <div className="carousel-inner">
+    <div className="item active">
+      <img src="http://placehold.it/1200x315" alt="..." />
+      <div className="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+    <div className="item">
+      <img src="http://placehold.it/1200x315" alt="..." />
+      <div className="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+    <div className="item">
+      <img src="http://placehold.it/1200x315" alt="..." />
+      <div className="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+  </div>
+
+
+  <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span className="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span className="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
       </div>
     )
   }
